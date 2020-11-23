e42f481ea1988ee8b3ef2eaf52a4298b2185da1d
denys.vedernikov <denysvedernikov333@gmail.com>
2020-11-23 10:25:25 +0200

    my first commit

--- main.c
+++ main.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
-#include <math.h>
 #include <conio.h>
+#include <string.h>
 
-int main() {
-   float x, res;
-   float e = 2.7f;
-   
-   
-for(x=4; x<=6; x += 0.2) {
-if(x<4.5f) {
-    res = 1/(sin(pow(x,2)));
-    printf ("The result for x=%.1f is: %f\n", x, res);
-    
 
-} 
-else if(x>=4.5f) {
-    if(x<5){
-      res = x + log(sqrt(pow(x,7)));
-      printf ("The result for x=%.1f is: %f\n", x, res);
-    }
-    
-    if(x>=5) {
-    res = log10(pow(e,x) + 4);
-    printf ("The result for x=%.1f is: %f\n", x, res);
-   
-      }
-  }
+void reverseString(char* word){
+int len = strlen(word);
+for(int i = len - 1; i >=0; i--){
+    printf("%c", word[i]);
+}
 }
 
 
- getch();
- return 0;
+int main(){
+    reverseString("Hello, world!");
+    getch();
+    return 0;
 }
